9479c5f35122baa239dd9f96a28f20b17d0daa25
Iqbal Dwiansyah <iqbaldwiansyah@gmail.com>
2025-09-13 07:50:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function showPopup(duration, price) {
     const popupOverlay = document.getElementById("popupOverlay");
     const popupText = document.getElementById("popupText");
 
-    popupText.textContent = `Yakin kamu memilih durasi ${duration} dengan harga ${price}?`;
+    popupText.textContent = `Yakin kamu memilih durasi ${duration} dengan harga ${price}?\n
+    Apabila sudah selesai pembayaran , Silahkan hubungi Tiktok/Discord Iqbal Dwiansyah`;
     popupOverlay.style.display = "flex";
 
     // Tombol Ya, saya setuju → hanya buka Sociabuzz di tab baru
